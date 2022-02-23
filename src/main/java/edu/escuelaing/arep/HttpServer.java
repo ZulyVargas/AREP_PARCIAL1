ae0259195b6ed71392ffcc966b21e214b37603d9
unknown <zuly.vargas@labinfo.is.escuelaing.edu.co>
2022-02-23 16:37:34 -0500

    Agregando lector puerto

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -7,10 +7,10 @@ import java.io.*;
 
 public class HttpServer {
 
-    public void main(String[] args) throws IOException {
+    public static void  main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -49,7 +49,7 @@ public class HttpServer {
                     + "<title>Title of the document</title>\n"
                     + "</head>"
                     + "<body>"
-                    + "My Web Site"
+                    + "Clima API"
                     + "</body>"
                     + "</html>";
             out.println(outputLine);
@@ -58,6 +58,14 @@ public class HttpServer {
             clientSocket.close();
         }
         serverSocket.close();
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set
+
     }
 }
 
